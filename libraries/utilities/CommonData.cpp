37adcedb2a08e3439c69c0d316c7cef4252a0ea2
HackFisher <hackfisher@gmail.com>
2017-09-23 15:36:52 +0800

    add support for validating ethereum address, Fixed #34

---
+++ libraries/utilities/CommonData.cpp
@@ -0,0 +1,100 @@
+#include <graphene/utilities/CommonData.h>
+#include <boost/algorithm/string.hpp>
+#include <graphene/utilities/SHA3.h>
+
+using namespace std;
+using namespace graphene::utilities;
+
+int graphene::utilities::fromHex(char _i, WhenError _throw)
+{
+	if (_i >= '0' && _i <= '9')
+		return _i - '0';
+	if (_i >= 'a' && _i <= 'f')
+		return _i - 'a' + 10;
+	if (_i >= 'A' && _i <= 'F')
+		return _i - 'A' + 10;
+	if (_throw == WhenError::Throw)
+		return -1;// BOOST_THROW_EXCEPTION(BadHexCharacter() << errinfo_invalidSymbol(_i));
+	else
+		return -1;
+}
+
+bytes graphene::utilities::fromHex(std::string const& _s, WhenError _throw)
+{
+	unsigned s = (_s.size() >= 2 && _s[0] == '0' && _s[1] == 'x') ? 2 : 0;
+	std::vector<uint8_t> ret;
+	ret.reserve((_s.size() - s + 1) / 2);
+
+	if (_s.size() % 2)
+	{
+		int h = fromHex(_s[s++], WhenError::DontThrow);
+		if (h != -1)
+			ret.push_back(h);
+		else if (_throw == WhenError::Throw)
+			return bytes();//BOOST_THROW_EXCEPTION(BadHexCharacter());
+		else
+			return bytes();
+	}
+	for (unsigned i = s; i < _s.size(); i += 2)
+	{
+		int h = fromHex(_s[i], WhenError::DontThrow);
+		int l = fromHex(_s[i + 1], WhenError::DontThrow);
+		if (h != -1 && l != -1)
+			ret.push_back((byte)(h * 16 + l));
+		else if (_throw == WhenError::Throw)
+			return bytes();//BOOST_THROW_EXCEPTION(BadHexCharacter());
+		else
+			return bytes();
+	}
+	return ret;
+}
+
+bool graphene::utilities::passesAddressChecksum(string const& _str, bool _strict)
+{
+    if (_str.substr(0, 2) != "0x")
+        return false;
+
+    // string s = _str.substr(0, 2) == "0x" ? _str.substr(2) : _str;
+    string s = _str.substr(2);
+
+    if (s.length() != 40)
+        return false;
+
+        /*
+    if (!_strict && (
+        _str.find_first_of("abcdef") == string::npos ||
+        _str.find_first_of("ABCDEF") == string::npos
+    ))
+        return true;
+        */
+    if (!_strict)
+    {
+        if (s.find_first_of("abcdef") == string::npos && s.find_first_not_of("0123456789ABCDEF") == string::npos)
+        {
+            return true;
+        } else if (s.find_first_of("ABCDEF") == string::npos && s.find_first_not_of("0123456789abcdef") == string::npos)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    h256 hash = keccak256(boost::algorithm::to_lower_copy(s, std::locale::classic()));
+    for (size_t i = 0; i < 40; ++i)
+    {
+        char addressCharacter = s[i];
+        bool lowerCase;
+        if ('a' <= addressCharacter && addressCharacter <= 'f')
+            lowerCase = true;
+        else if ('A' <= addressCharacter && addressCharacter <= 'F')
+            lowerCase = false;
+        else
+            continue;
+        unsigned nibble = (unsigned(hash[i / 2]) >> (4 * (1 - (i % 2)))) & 0xf;
+        if ((nibble >= 8) == lowerCase)
+            return false;
+    }
+    return true;
+}
---
+++ tests/ethereum/etheraddr_tests.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2015 Cryptonomex, Inc., and contributors.
+ *
+ * The MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+ #include <boost/test/unit_test.hpp>
+ 
+ #include <graphene/utilities/CommonData.h>
+ 
+ using namespace graphene::utilities;
+ 
+ BOOST_AUTO_TEST_SUITE( ether_address_tests )
+ 
+ BOOST_AUTO_TEST_CASE( non_checksum_test )
+ {
+    BOOST_CHECK(passesAddressChecksum("0x416f9f8fa0f2e133ad7e1ff52a85e4fbc2b00253", false));
+
+    BOOST_CHECK(!passesAddressChecksum("0x416f9f8fa0f2e133ad7e1ff52a85e4fbc2b0025x", false));
+ }
+
+ BOOST_AUTO_TEST_CASE( serialization_json_test )
+ {
+    BOOST_CHECK(passesAddressChecksum("0x416f9f8fa0f2E133ad7e1FF52a85e4fBC2B00253", true));
+
+    BOOST_CHECK(!passesAddressChecksum("0x416F9f8fa0f2E133ad7e1FF52a85e4fBC2B00253", true));
+ }
+ 
+ BOOST_AUTO_TEST_SUITE_END()
+ 
+ //#define BOOST_TEST_MODULE "C++ Unit Tests for Graphene Blockchain Database"
+#include <cstdlib>
+#include <iostream>
+#include <boost/test/included/unit_test.hpp>
+
+boost::unit_test::test_suite* init_unit_test_suite(int argc, char* argv[]) {
+   std::srand(time(NULL));
+   std::cout << "Random number generator seeded to " << time(NULL) << std::endl;
+   return nullptr;
+}
